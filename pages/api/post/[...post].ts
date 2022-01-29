02040580fa262f6e634b20d02bce2faa61226e96
saile515 <elias.jorgensen2006@gmail.com>
2022-01-29 15:29:37 +0100

    Moved blogposts to database

--- pages/api/post/[...post].ts
+++ pages/api/post/[...post].ts
@@ -4,7 +4,7 @@ const path = require("path");
 
 export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
 	return new Promise((resolve, reject) => {
-		fs.readFile("./Blogposts\\" + req.query.post[0] + ".html", "utf8", (err: Error, data: string) => {
+		fs.readFile(`${process.env.MD_OUTPUT}/${req.query.post[0]}.html`, "utf8", (err: Error, data: string) => {
 			if (err) throw err;
 			res.send(data);
 			resolve(true);
