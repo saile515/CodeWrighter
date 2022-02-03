3ef5834d480a551d848176332e6977582daa6705
saile515 <elias.jorgensen2006@gmail.com>
2022-02-03 20:05:19 +0100

    Do again, do right

--- pages/post/[...post].tsx
+++ pages/post/[...post].tsx
@@ -1,24 +1,11 @@
 import type { NextPage } from "next";
 import styles from "../../styles/Post.module.css";
 import { useRouter } from "next/router";
-import { Blogpost } from "../../Components/Blogpost";
-import { Hierarchy } from "../../Components/Hierarchy";
-import { Metadata } from "../../Components/Metadata";
 
 const Home: NextPage = () => {
 	const router = useRouter();
 
-	return (
-		<div>
-			{router.query.post && (
-				<div className={styles.container}>
-					<Blogpost url={`/api/post/${router.query.post[0]}`} />
-					<Metadata url={`/api/metadata/${router.query.post[0]}`} />
-					<Hierarchy />
-				</div>
-			)}
-		</div>
-	);
+	return <div></div>;
 };
 
 export default Home;
