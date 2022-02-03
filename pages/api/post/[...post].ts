4d03a9b479ca53ece5b75be2ddea526a29538f66
saile515 <elias.jorgensen2006@gmail.com>
2022-02-03 20:06:47 +0100

    Reset api routes

--- pages/api/post/[...post].ts
+++ pages/api/post/[...post].ts
@@ -3,11 +3,5 @@ const fs = require("fs");
 const path = require("path");
 
 export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
-	return new Promise((resolve, reject) => {
-		fs.readFile(`${process.env.MD_OUTPUT}/${req.query.post[0]}.html`, "utf8", (err: Error, data: string) => {
-			if (err) throw err;
-			res.send(data);
-			resolve(true);
-		});
-	});
+	return new Promise((resolve, reject) => {});
 }
