83dc86cd0fb02be8183f5259839cdc5fabb64d22
saile515 <elias.jorgensen2006@gmail.com>
2022-02-06 13:46:51 +0100

    Table of contents highlighting

--- Components/TableOfContents.tsx
+++ Components/TableOfContents.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useEffect, useState } from "react";
+import { ReactElement, useEffect, useRef, useState } from "react";
 import styles from "../styles/TableOfContents.module.css";
 
 interface HeadingInterface {
@@ -44,13 +44,54 @@ function getHeadings() {
 	return structure;
 }
 
-function createTableOfContents(structure: HeadingInterface[], depth: number) {
+function useIntersectionObserver(setActiveId: any, ready: boolean) {
+	const headingElementsRef: any = useRef({});
+	useEffect(() => {
+		function callback(headings: IntersectionObserverEntry[]) {
+			headingElementsRef.current = headings.reduce((map: any, headingElement) => {
+				map[headingElement.target.id] = headingElement;
+				return map;
+			}, headingElementsRef.current);
+
+			const visibleHeadings: any = [];
+			Object.keys(headingElementsRef.current).forEach((key) => {
+				const headingElement = headingElementsRef.current[key];
+				if (headingElement.isIntersecting) visibleHeadings.push(headingElement);
+			});
+
+			function getIndexFromId(id: any) {
+				headingElements.findIndex((heading) => heading.id === id);
+			}
+
+			if (visibleHeadings.length === 1) {
+				setActiveId(visibleHeadings[0].target.id);
+			} else if (visibleHeadings.length > 1) {
+				const sortedVisibleHeadings = visibleHeadings.sort((a: any, b: any) => {
+					return getIndexFromId(a.target.id) > getIndexFromId(b.target.id);
+				});
+				setActiveId(sortedVisibleHeadings[0].target.id);
+			}
+		}
+
+		const observer = new IntersectionObserver(callback, {
+			rootMargin: "0px 0px -40% 0px",
+		});
+
+		const headingElements = Array.from(document.querySelectorAll("h2, h3"));
+
+		headingElements.forEach((element) => observer.observe(element));
+
+		return () => observer.disconnect();
+	}, [setActiveId, ready]);
+}
+
+function createTableOfContents(structure: HeadingInterface[], depth: number, active: any) {
 	const headings: ReactElement[] = [];
 	structure.forEach((heading) => {
 		headings.push(
 			<li key={heading.id}>
 				<a
-					className={` transition-all duration-300 hover:pl-2 ${depth == 1 ? "font-bold text-xl text-white" : " text-slate-400"}`}
+					className={` transition-[padding] duration-300 hover:pl-2 ${depth == 1 ? "font-bold text-xl text-white" : `${heading.id == active ? "text-white" : "text-slate-400"}`}`}
 					href={`#${heading.id}`}
 					onClick={(event) => {
 						event.preventDefault();
@@ -58,7 +99,7 @@ function createTableOfContents(structure: HeadingInterface[], depth: number) {
 					}}>
 					{heading.name}
 				</a>
-				{heading.body && <ul className=" pl-4 list-none">{createTableOfContents(heading.body, depth + 1)}</ul>}
+				{heading.body && <ul className=" pl-4 list-none">{createTableOfContents(heading.body, depth + 1, active)}</ul>}
 			</li>
 		);
 	});
@@ -67,10 +108,12 @@ function createTableOfContents(structure: HeadingInterface[], depth: number) {
 }
 
 export default function TableOfContents(props: { ready: boolean }) {
+	const [activeId, setActiveId] = useState();
 	const [structure, setStructure] = useState<HeadingInterface[]>();
+	useIntersectionObserver(setActiveId, props.ready);
 
 	useEffect(() => {
 		setStructure(getHeadings());
 	}, [props.ready]);
-	return <nav className={`${styles.container} bg-slate-900 border-l-2 border border-solid border-slate-800`}>{structure && createTableOfContents(structure as HeadingInterface[], 1)}</nav>;
+	return <nav className={`${styles.container} bg-slate-900 border-l-2 border border-solid border-slate-800`}>{structure && createTableOfContents(structure as HeadingInterface[], 1, activeId)}</nav>;
 }
