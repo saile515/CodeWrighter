6614c3ea64da7c63fe342f61784d553aa758ebe7
saile515 <elias.jorgensen2006@gmail.com>
2022-02-06 17:04:10 +0100

    Mobile adjustments

--- Components/PostTile.tsx
+++ Components/PostTile.tsx
@@ -2,11 +2,11 @@ import { PostData } from "./PostBrowser";
 
 export default function PostTile(props: PostData) {
 	return (
-		<div className=" bg-slate-800 text-gray-400 p-2 rounded-md shadow-[0_1rem_1.5rem_-0.8rem] shadow-neutral-900 relative">
+		<a className="relative bg-slate-800 text-gray-400 p-2 rounded-md shadow-[0_1rem_1.5rem_-0.8rem] shadow-neutral-900" href={`/post/${props.id}`}>
 			<p className=" text-2xl font-bold text-white">{props.name}</p>
 			<p>Written {new Date(props.date).toLocaleDateString(undefined, { year: "numeric", month: "long", day: "numeric" })}</p>
 			{props.edits[0] && <p>Edited {new Date(props.edits.slice(-1)[0]).toLocaleDateString(undefined, { year: "numeric", month: "long", day: "numeric" })}</p>}
 			<p className=" text-slate-700 bottom-2 right-2 absolute">{props.id}</p>
-		</div>
+		</a>
 	);
 }
