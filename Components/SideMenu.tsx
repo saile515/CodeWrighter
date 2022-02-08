107804cfad113e4c9d37ca173fa961a5ba9f48e5
saile515 <elias.jorgensen2006@gmail.com>
2022-02-08 13:54:11 +0100

    Search bar (front end)

--- Components/SideMenu.tsx
+++ Components/SideMenu.tsx
@@ -1,5 +1,16 @@
 import styles from "../styles/SideMenu.module.css";
+import GitHubFeed from "./GitHubFeed";
+import Navigation from "./Navigation";
+import SearchBar from "./SearchBar";
+import SocialList from "./SocialList";
 
 export default function SideMenu() {
-	return <div className={`${styles.container} bg-slate-900 border-r-2 border border-solid border-slate-800`}></div>;
+	return (
+		<div className={`${styles.container} bg-slate-900 border-r-2 border border-solid border-slate-800`}>
+			<SearchBar />
+			<Navigation />
+			<GitHubFeed />
+			<SocialList />
+		</div>
+	);
 }
---
+++ Components/SocialList.tsx
@@ -0,0 +1,3 @@
+export default function SocialList() {
+	return <div></div>;
+}
