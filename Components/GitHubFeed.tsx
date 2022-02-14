76f9893042b61ffb53261f12a68e0b77abdc7605
saile515 <elias.jorgensen2006@gmail.com>
2022-02-14 19:11:12 +0100

    Added links to github feed

--- Components/GitHubFeed.tsx
+++ Components/GitHubFeed.tsx
@@ -13,11 +13,20 @@ interface Commit {
 function GitHubItem(props: { data: Commit }) {
 	return (
 		<li className=" bg-slate-800 p-2 my-1 rounded-lg">
-			<p className=" text-white m-0 font-bold overflow-ellipsis whitespace-nowrap overflow-hidden">{props.data.message}</p>
-			<p className=" m-0 text-slate-500">
-				By {props.data.author} in {props.data.repo}
-			</p>
-			<p className=" m-0 text-slate-500">{new Date(props.data.date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })}</p>
+			<a href={props.data.url} target="_blank">
+				<p className=" text-white m-0 font-bold overflow-ellipsis whitespace-nowrap overflow-hidden">{props.data.message}</p>
+				<p className=" m-0 text-slate-500">
+					By{" "}
+					<a href={`https://www.github.com/${props.data.author}`} target="_blank" className=" hover:text-slate-400 transition-all">
+						{props.data.author}
+					</a>{" "}
+					in{" "}
+					<a href={`https://www.github.com/${props.data.repo}`} target="_blank" className=" hover:text-slate-400 transition-all">
+						{props.data.repo}
+					</a>
+				</p>
+				<p className=" m-0 text-slate-500">{new Date(props.data.date).toLocaleDateString("en-US", { year: "numeric", month: "long", day: "numeric" })}</p>
+			</a>
 		</li>
 	);
 }
