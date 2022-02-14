7bad4020b034a2908d27a1134aade9d39363b2cd
saile515 <elias.jorgensen2006@gmail.com>
2022-02-14 20:39:57 +0100

    Added title (name temporary)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
 import PostBrowser from "../Components/PostBrowser";
 import SideMenu from "../Components/SideMenu";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
 	return (
 		<div className={styles.container}>
 			<SideMenu />
-			<PostBrowser />
+			<div className="grid grid-rows-[4rem_1fr] bg-slate-900">
+				<h1 className="text-5xl border-b-2 border-slate-800 my-0 mx-4 text-slate-400 leading-[4rem]">codeplot.net</h1>
+				<PostBrowser />
+			</div>
 		</div>
 	);
 };
