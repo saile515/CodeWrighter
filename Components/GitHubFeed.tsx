a1e8e0c1917d77c4d75bbe5903959343e9c33db6
saile515 <elias.jorgensen2006@gmail.com>
2022-02-14 20:34:09 +0100

    Added rel no referrer to offsite links

--- Components/GitHubFeed.tsx
+++ Components/GitHubFeed.tsx
@@ -13,15 +13,15 @@ interface Commit {
 function GitHubItem(props: { data: Commit }) {
 	return (
 		<li className=" bg-slate-800 p-2 my-1 rounded-lg">
-			<a href={props.data.url} target="_blank">
+			<a href={props.data.url} target="_blank" rel="noreferrer">
 				<p className=" text-white m-0 font-bold overflow-ellipsis whitespace-nowrap overflow-hidden">{props.data.message}</p>
 				<p className=" m-0 text-slate-500">
 					By{" "}
-					<a href={`https://www.github.com/${props.data.author}`} target="_blank" className=" hover:text-slate-400 transition-all">
+					<a href={`https://www.github.com/${props.data.author}`} target="_blank" rel="noreferrer" className=" hover:text-slate-400 transition-all">
 						{props.data.author}
 					</a>{" "}
 					in{" "}
-					<a href={`https://www.github.com/${props.data.repo}`} target="_blank" className=" hover:text-slate-400 transition-all">
+					<a href={`https://www.github.com/${props.data.repo}`} target="_blank" rel="noreferrer" className=" hover:text-slate-400 transition-all">
 						{props.data.repo}
 					</a>
 				</p>
--- Components/SocialList.tsx
+++ Components/SocialList.tsx
@@ -5,13 +5,13 @@ export default function SocialList() {
 	const socialTW = " w-8";
 	return (
 		<div className=" flex justify-evenly">
-			<a className={`${socialTW} ${styles.social}`} href="https://www.github.com/saile515" target="_blank">
+			<a className={`${socialTW} ${styles.social}`} href="https://www.github.com/saile515" target="_blank" rel="noreferrer">
 				<Image src="/github.png" alt="Github Logo" layout="responsive" width={64} height={64} />
 			</a>
-			<a className={`${socialTW} ${styles.social}`} href="https://www.reddit.com/u/saile515" target="_blank">
+			<a className={`${socialTW} ${styles.social}`} href="https://www.reddit.com/u/saile515" target="_blank" rel="noreferrer">
 				<Image src="/reddit.png" alt="Reddit Logo" layout="responsive" width={64} height={64} />
 			</a>
-			<a className={`${socialTW} ${styles.social}`} href="https://open.spotify.com/user/musicman515?si=73d7eb3c1c96473a" target="_blank">
+			<a className={`${socialTW} ${styles.social}`} href="https://open.spotify.com/user/musicman515?si=73d7eb3c1c96473a" target="_blank" rel="noreferrer">
 				<Image src="/spotify.png" alt="Spotify Logo" layout="responsive" width={64} height={64} />
 			</a>
 		</div>
